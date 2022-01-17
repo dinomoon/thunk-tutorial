aab37a309c92206791d2429e22919d7057e32535
dinomoon <ansrud45@gmail.com>
2022-01-17 18:37:26 +0900

    Update App

--- src/App.js
+++ src/App.js
@@ -28,14 +28,13 @@ function App() {
     }
   }, [cart, dispatch]);
 
-  console.log(cart);
   return (
     <>
       {notification && (
         <Notification
           status={notification.status}
           title={notification.title}
-          message={notification.title}
+          message={notification.message}
         />
       )}
       <Layout>
